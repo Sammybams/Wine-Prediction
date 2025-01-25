6727f4778df08336bd53e8e2b65e6310780544df
Sammybams <samuelbamgbola@gmail.com>
2025-01-25 21:49:08 +0100

    Created starter script for fastapi deployment.

---
+++ main.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Entry(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Project": "Wine Prediction Project."}
+
+
+@app.post("/prediction")
+def make_prediction(entry: Entry):
+    # return {"item_id": item_id, "q": q}
+
