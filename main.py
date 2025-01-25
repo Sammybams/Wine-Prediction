d464cc313350114a8bf295227133f2f2dbdcbae4
Sammybams <samuelbamgbola@gmail.com>
2025-01-25 22:18:36 +0100

    Added loading of model and making predictions to app.

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ from typing import Union
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pickle
+from sklearn.ensemble import RandomForestRegressor
 
 app = FastAPI()
 
 
 class Entry(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
+    fixed_acidity: float
+    volatile_acidity: float
+    citric_acid: float
+    redisual_sugar: float
+    chlorides: float
+    free_sulfur_dioxide: float
+    total_sulfur_dioxide: float
+    density: float
+    pH: float
+    sulphates: float
+    alcohol: float
 
 @app.get("/")
 def read_root():
@@ -19,5 +28,8 @@ def read_root():
 
 @app.post("/prediction")
 def make_prediction(entry: Entry):
-    return
-    # return {"item_id": item_id, "q": q}
+    model = pickle.load(open('../model/wine_rmodel.pkl', 'rb'))
+    test = [[entry.fixed_acidity, entry.volatile_acidity, entry.citric_acid,
+             entry.redisual_sugar, entry.chlorides, entry.free_sulfur_dioxide,
+             entry.total_sulfur_dioxide, entry.density, entry.pH, entry.sulphates, entry.alcohol]]
+    return model.predict([test])[0]
